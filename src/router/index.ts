8594625ef938fe58b8862452b00008d19ffa27d3
allenalaee101 <ala.2000.amir@gmail.com>
2025-11-19 16:09:07 +0330

    starting FOS

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,14 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+import AboutView from "../views/AboutView.vue";
+const routes = [
+  // { path: "/", component: Home }, ...
+  { path: "/", name: "home", component: HomeView },
+  { path: "/about", name: "about", component: AboutView },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
